66ab2ad41d0c10a350d6fe594569dcbaabe9274d
Andreas Cederström <andreas.cederstrom@factor10.com>
2021-05-28 13:40:31 +0200

    green 🥬

--- src/Fibonacci.ts
+++ src/Fibonacci.ts
@@ -1,6 +1,6 @@
 export class Fibonacci {
   static gief(start: number): number {
     if (start <= 1) return start;
-    return start - 1;
+    return Fibonacci.gief(start - 1) + Fibonacci.gief(start - 2);
   }
 }
--- test/Fibonacci.test.ts
+++ test/Fibonacci.test.ts
@@ -5,3 +5,4 @@ test("1", () => expect(Fibonacci.gief(1)).toBe(1));
 test("2", () => expect(Fibonacci.gief(2)).toBe(1));
 test("3", () => expect(Fibonacci.gief(3)).toBe(2));
 test("4", () => expect(Fibonacci.gief(4)).toBe(3));
+test("5", () => expect(Fibonacci.gief(5)).toBe(5));
