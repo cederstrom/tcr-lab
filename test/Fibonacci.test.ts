b7ac039b681256dbf7aea64f605719af7dabc591
Andreas Cederström <andreas.cederstrom@factor10.com>
2021-05-28 13:34:05 +0200

    green 🥬

--- test/Fibonacci.test.ts
+++ test/Fibonacci.test.ts
@@ -1,13 +1,5 @@
 import { Fibonacci } from "src/Fibonacci";
 
-test("0", () => {
-  expect(Fibonacci.gief(0)).toBe(0);
-});
-
-test("1", () => {
-  expect(Fibonacci.gief(1)).toBe(1);
-});
-
-test("2", () => {
-  expect(Fibonacci.gief(2)).toBe(1);
-});
+test("0", () => expect(Fibonacci.gief(0)).toBe(0));
+test("1", () => expect(Fibonacci.gief(1)).toBe(1));
+test("2", () => expect(Fibonacci.gief(2)).toBe(1));
